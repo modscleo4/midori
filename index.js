4d2564a4e4327c5d0d1190ed482c9cc726793503
Dhiego Cassiano Fogaça Barbosa <modscleo4@outlook.com>
2022-08-02 06:27:07 -0300

    add Server class

--- index.js
+++ index.js
@@ -1,30 +1,8 @@
 import routes from './routes/index.js';
-import {createServer} from 'http';
-import Request from "./lib/Request.js";
-import Response from "./lib/Response.js";
+import Server from "./lib/Server.js";
 
-const server = createServer(async (req, res) => {
-    const request = new Request(req);
-    const response = new Response(res);
-
-    if (!routes.some(r => r.path === request.path)) {
-        response.status(404);
-    } else {
-        const route = routes.find(route => route.method === request.method && route.path === request.path);
-
-        if (route) {
-            try {
-                await route.handle(request, response);
-            } catch (e) {
-                console.error(e);
-                response.status(500);
-            }
-        } else {
-            response.status(405);
-        }
-    }
-
-    res.end();
+const server = new Server({
+    routes
 });
 
 const port = 3000;
